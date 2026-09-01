55f44c723b378899ba9f1b9eb0eec2e1bc47d232
themazharhussain <mazherhussain926@gmail.com>
2026-09-01 11:30:58 +0500

    Operators

---
+++ 04_js/js-learning/03_operators.js
@@ -0,0 +1,60 @@
+// Operators 
+// Operator simply takes value as operand, perform some actions on operands and finally give final output
+// Operators may be unary or binary
+// Unary operators only takes one operand while binary takes two operand
+
+// Arithmetic ==> + - * / % ** (Mostly used in maths)
+
+// Assignment ==>  = += -= *= /=  a++ a-- (These are used to assign values to variables)
+
+// Comparison    >  >=  <  <=  !== !=    (These are used to compare values)
+
+// Logical  &&(and)  ||(or) !(not) (These are used to compare two or more conditions)
+
+// Type conversion (We can convert one datatype to another datatype in js)
+
+// String to number  String()
+// Number to string Number() 
+// parseInt and parsFloat   parseInt() parseFloat()
+
+// == vs === (double equal only compare values not type while triple equal compare values as well as types too)
+
+// Falsy values===>   false, 0, "", null, undefined, NaN,
+
+// Arithmetic operator
+let a = 10;
+let b = 2;
+
+//console.log(a + b)
+//console.log(a - b)
+//console.log(a * b)
+//console.log(a / b)
+//This is called modulus operator. It always give us remainder as output
+//console.log(a % b)
+// Exponential operator
+//console.log(a ** b)
+
+
+// Assignment operator
+
+a += 10  // a = a + 10
+a -= 10  // a = a - 10
+a *= 10  // a = a * 10
+a /= 10  // a = a / 10
+
+//console.log(a)
+
+// Increment and decrement operators
+
+let x = 10;
+x++; // x = x + 1
+x--; // x = x - 1
+
+//console.log(x)
+
+//Comparison operators
+
+
+
+console.log(10 > 20)
+console.log(10 >= 10)
