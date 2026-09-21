2708d5dc6c5d21f39c914523b5879692ac281f11
themazharhussain <mazherhussain926@gmail.com>
2026-09-21 11:36:51 +0500

    modules

---
+++ 04_js/modules_learning/main.js
@@ -0,0 +1,7 @@
+import { subtractNumbers } from "./maths.js"
+
+import multiply from "./maths.js"
+
+import { divide } from "./maths.js"
+
+console.log(divide(4, 5))
---
+++ 04_js/modules_learning/maths.js
@@ -0,0 +1,38 @@
+// export function add(a, b) {
+//     return a + b
+// }
+
+// export const subtractNumbers = (a, b) => {
+//     return a - b
+// }
+
+// export default function multiply(a, b) {
+//     return a * b
+// }
+
+// export function divide(a, b) {
+//     return a / b
+// }
+
+// export function add(a, b) {
+//     return a + b
+// }
+
+
+function add(a, b) {
+    return a + b
+}
+
+const subtractNumbers = (a, b) => {
+    return a - b
+}
+
+export default function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+export { add, subtractNumbers, divide }
